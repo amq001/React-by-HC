d10e94640a231978c68fd3407ad440fcfa0dc293
amq001 <aqibmehtab001@gmail.com>
2024-04-14 17:46:14 +0500

    Blog app initial commit

---
+++ 12BlogApp/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  )
+}
+
+export default App
